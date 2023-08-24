97130237fcc89be4c99d7fcae23a23f6b52ad36e
jesgarlan <96166634+jmgarcia-96@users.noreply.github.com>
2023-08-24 21:08:15 +0200

    Renovacion proyecto

--- src/app/pipes/filter-tabs.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterTabsPipe } from './filter-tabs.pipe';
-
-describe('FilterTabsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterTabsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
